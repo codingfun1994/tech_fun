72984d358ea0a54ee14b3fb57c27d11c6ee269e2
codingfun1994 <codingfun1994@gmail.com>
2025-04-21 00:04:35 +0300

    add asset files

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as lockOpenIcon } from "./lock-open.svg";
+export { default as lockClosedIcon } from "./lock-closed.svg";
+export { default as sunIcon } from "./sun.svg";
+export { default as moonIcon } from "./black-moon.svg";
+export { default as refreshIcon } from "./refresh.svg";
+export { default as spinnerIcon } from "./spinner.svg";
